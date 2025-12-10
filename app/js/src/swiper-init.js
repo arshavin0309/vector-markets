04c5fd2624191df44ca8ef8db24bc9e1c0006bc2
Владислав <ivanov.vladislav.yurevich@gmail.com>
2025-12-10 17:15:13 +0500

    добавил несколько секций

--- app/js/src/swiper-init.js
+++ app/js/src/swiper-init.js
@@ -16,3 +16,17 @@ let mainPlatformSwiper = new Swiper(".main-platform__swiper", {
         swiper: mainPlatformButtons,
     },
 });
+
+let mainAnalyticsButtons = new Swiper(".main-analytics__buttons", {
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+
+let mainAnalyticsSwiper = new Swiper(".main-analytics__swiper", {
+    spaceBetween: 100,
+
+    thumbs: {
+        swiper: mainAnalyticsButtons,
+    },
+});
