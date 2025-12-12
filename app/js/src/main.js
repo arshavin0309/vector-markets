e395a977a99b6785cd7d880a6da00d6b1f2119e0
Владислав <ivanov.vladislav.yurevich@gmail.com>
2025-12-12 12:23:55 +0500

    доработка десктопа

--- app/js/src/main.js
+++ app/js/src/main.js
@@ -1,13 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const links = document.querySelectorAll('a[href="#contact"]');
-    const target = document.querySelector('.main-contacts');
+    const mainContacts = document.querySelector('.main-contacts');
+    const mainContactsClose = document.querySelector('.main-contacts__close');
 
-    if (target) {
+    if (mainContacts) {
         links.forEach(link => {
             link.addEventListener('click', e => {
                 e.preventDefault();
-                target.classList.toggle('show');
+                mainContacts.classList.toggle('show');
             });
         });
     }
+
+    function closeForm() {
+        mainContacts.classList.remove('show');
+    }
+
+    mainContactsClose.addEventListener('click', closeForm);
+
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'Escape') {
+            closeForm();
+        }
+    });
+
+    // mainContacts.classList.add('show'); //тест
 });
